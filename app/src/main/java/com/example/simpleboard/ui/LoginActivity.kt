89a5aa2a1dc883ca8b79d4945c3363fde5c3fa73
habilithm97 <habinkim97@gmail.com>
2024-08-31 23:50:33 +0900

    로그인 화면

--- app/src/main/java/com/example/simpleboard/ui/LoginActivity.kt
+++ app/src/main/java/com/example/simpleboard/ui/LoginActivity.kt
@@ -1,20 +1,30 @@
-package com.example.simpleboard
+package com.example.simpleboard.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.simpleboard.databinding.ActivityLoginBinding
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        init()
+    }
+
+    private fun init() {
+        binding.apply {
+
         }
     }
 }
