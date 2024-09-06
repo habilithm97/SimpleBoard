5f2a0e142be5984ba64532157581f8ed1c5c7796
habilithm97 <habinkim97@gmail.com>
2024-09-06 22:35:45 +0900

    툴바 뒤로가기 버튼

--- app/src/main/java/com/example/simpleboard/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/simpleboard/ui/RegisterActivity.kt
@@ -1,11 +1,12 @@
 package com.example.simpleboard.ui
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.simpleboard.R
 import com.example.simpleboard.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
@@ -25,7 +26,19 @@ class RegisterActivity : AppCompatActivity() {
 
     private fun init() {
         binding.apply {
+            setSupportActionBar(toolbar)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true) // 뒤로가기 버튼 활성화
+            toolbar.navigationIcon?.setTint(Color.WHITE)
+        }
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
